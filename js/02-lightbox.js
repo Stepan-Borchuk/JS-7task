1da797f5eabdf6dd2973168d66271498eab549b5
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-04-24 20:17:40 +0300

    2ok

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryItemsList = document.querySelector(".gallery") 
+
+
+ let galleryList = [];
+
+    galleryItems.forEach(item => {
+        const galleryLink = document.createElement('a');
+        galleryLink.classList.add('gallery__item');
+        galleryLink.href = `${item.original}`;
+
+        const galleryImage = document.createElement('img');
+        galleryImage.classList.add('gallery__image');
+        galleryImage.src = `${item.preview}`;
+        galleryImage.alt = `${item.description}`;
+
+        galleryLink.append(galleryImage);
+        
+        galleryList.push(galleryLink);
+    }
+    )
+
+galleryItemsList.append(...galleryList)    
+    
+galleryItemsList.addEventListener('click', onClick);
+
+function onClick(evt) {
+    evt.preventDefault();
+
+    new SimpleLightbox('.gallery a', {captionDelay: 250, captionsData: 'alt'});
+}
